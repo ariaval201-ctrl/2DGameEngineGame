31f628577a2b133c47c68c410717c5d34d1c3c8f
ariaval201-ctrl <ariaval201@gmail.com>
2026-03-04 15:22:54 +0000

    level beta and code fixes

--- Assets/FishingRod.cs
+++ Assets/FishingRod.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class FishingRod : MonoBehaviour
 {
     public GameObject hookPrefab;
     public Transform hookSpawnPoint;
     public float hookSpeed = 5f;
+    public CameraManager cameraManager;
+    public CinemachineCamera myCamera;
 
     void Update()
-    {
+    {   
+    if(cameraManager.currentCam == myCamera)
         if (Input.GetKeyDown(KeyCode.E))
         {
             SpawnHook();
