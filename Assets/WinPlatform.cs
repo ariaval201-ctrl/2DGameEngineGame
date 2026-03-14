9c66f4e527c9da36da3056e9517ca74ddaa03355
ariaval201-ctrl <ariaval201@gmail.com>
2026-03-14 16:52:20 +0000

    Level made and Winscreen made

---
+++ Assets/WinPlatform.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinPlatform : MonoBehaviour
+{
+    private bool player1On = false;
+    private bool player2On = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            player1On = true;
+        }
+
+        if(other.CompareTag("Player2"))
+        {
+            player2On = true;
+        }
+
+        CheckWin();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            player1On = false;
+        }
+
+        if(other.CompareTag("Player2"))
+        {
+            player2On = false;
+        }
+    }
+
+    void CheckWin()
+    {
+        if(player1On && player2On)
+        {
+            
+    
+        SceneManager.LoadScene("WinScreen");
+    
+            
+        }
+    }
+}
